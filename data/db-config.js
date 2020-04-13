c6ef9078f9840b586ca32ba07468707fc8b3e9ed
Kristi Gribble <kgribble4083@gmail.com>
2020-04-12 21:32:29 -0500

    fixed db-config file

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,7 @@
 const knex = require("knex")
-const knexfile = require("../knexfile")
 
-const dbEnv = process.env.DB_ENV || "developement"
+const knexfile = require("../knexfile.js")
+
+const db = knex(config.development)
 
 module.exports = knex(knexfile[dbEnv]);
